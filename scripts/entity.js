ff2a6443fb9789eb3fc8266f4c40dbfceea6af9f
Bardes Rémi <rbardes@a-018818.ups.u-psud.fr>
2019-02-21 12:33:22 +0100

    squeleton of entitys

--- scripts/entity.js
+++ scripts/entity.js
@@ -3,31 +3,47 @@
  *  it represent every object inside a level of the bomberman :
  *  walls / destructible walls / foes / players / bombs
  *  Contains the common informations of every entities :
- *  
+ *  attributes :
+ *      x : the row index in the level.Map
+ *      y : he column index in the levelMap
+ *      texture : by default the ground texture, changed in each daughter class.
  */
 
 class Entity {
 
-    //construtors
+    /* CONSTRUCTORS */
     constructor(x, y){
         Object.defineProperty(this, "x", {value : x , writable : true});
         Object.defineProperty(this, "y", {value : y , writable : true});
-        Object.defineProperty(this, "texture", {value : "empty_texture" , writable : true});
+        Object.defineProperty(this, "texture", {value : "ground_texture" , writable : true});
     }
-    //methods (entity got no methods)
+    /* METHODS */ 
+
+    //
+    onDestroy(){};
 }
 
+/*
+ * Used for level's border walls && unbreakable walls
+ * Also mother of DestructibleWall
+ * different texture from Entity
+ */
 class Wall extends Entity{
-    //construtors
+    /* CONSTRUCTORS */
     constructor(x, y){
         super(x,y);
         this.texture = "wall_texture";
     }
-    //methods
+    //methods (Wall got no methods)
 }
 
+/*
+ * Used for DestructibleWalls inside level
+ * got different texture from Wall
+ * can be destroy
+ */
 class DestructibleWall extends Wall{
-    //construtors
+    /* CONSTRUCTORS */
     constructor(x, y){
         super(x,y);
         this.texture = "destructible_wall_texture";
@@ -35,8 +51,14 @@ class DestructibleWall extends Wall{
     //methods
 }
 
+/*
+ * Used for Bombs inside level
+ * got different texture from Entity
+ * can be destroy
+ * 
+ */
 class Bomb extends Entity{
-    //construtors
+    /* CONSTRUCTORS */
     constructor(x, y){
         super(x,y);
         this.texture = "bomb_texture";
@@ -44,18 +66,31 @@ class Bomb extends Entity{
     //methods
 }
 
+/*
+ *
+ *
+ * 
+ * 
+ */
 class MovingEntity extends Entity{
-    //constructors
+    /* CONSTRUCTORS */
     constructor(x, y){
         super(x,y);
+        Object.defineProperty(this, "direction", {value : "none" , writable : true, 
+                              get : () => {return this.direction},
+                              set : (s) => {this.direction = s} });
     }
-
     //methods
-
 }
 
+/*
+ *
+ *
+ * 
+ * 
+ */
 class Player extends MovingEntity{
-    //construtors
+    /* CONSTRUCTORS */
     constructor(x, y){
         super(x,y);
         this.texture = "face_player_texture";
@@ -63,8 +98,14 @@ class Player extends MovingEntity{
     //methods
 }
 
+/*
+ *
+ *
+ * 
+ * 
+ */
 class Foe extends MovingEntity{
-    //construtors
+    /* CONSTRUCTORS */
     constructor(x, y){
         super(x,y);
         this.texture = "face_foe_texture";
